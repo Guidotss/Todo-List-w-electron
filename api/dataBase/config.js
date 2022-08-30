7e9cec513c5deec64742dfbd377a6a2517796b20
Guidotss <100441914+Guidotss@users.noreply.github.com>
2022-08-30 17:40:53 -0300

    Delete api directory

--- api/dataBase/config.js
+++
@@ -1,8 +0,0 @@
-const config ={
-    url:`mongodb+srv://guido:guidomartin@cluster0.wtvdvhi.mongodb.net/taks?retryWrites=true&w=majority`,
-    options:{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }
-}
-module.exports = config;
